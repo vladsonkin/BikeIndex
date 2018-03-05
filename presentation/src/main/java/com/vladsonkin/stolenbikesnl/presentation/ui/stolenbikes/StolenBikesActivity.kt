353947d45b87f59ccce19106690c3c0c52e1910a
Sonkin Vladislav <vsonkin@imac-vsonkin.ozon>
2018-03-05 16:51:28 +0300

    dependencies, toolbar style

--- presentation/src/main/java/com/vladsonkin/stolenbikesnl/presentation/ui/stolenbikes/StolenBikesActivity.kt
+++ presentation/src/main/java/com/vladsonkin/stolenbikesnl/presentation/ui/stolenbikes/StolenBikesActivity.kt
@@ -1,13 +1,16 @@
-package com.vladsonkin.stolenbikesnl.presentation.stolenbikes
+package com.vladsonkin.stolenbikesnl.presentation.ui.stolenbikes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.vladsonkin.stolenbikesnl.R
+import kotlinx.android.synthetic.main.activity_stolen_bikes.*
 
 class StolenBikesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stolen_bikes)
+
+        setSupportActionBar(toolbar)
     }
 }
